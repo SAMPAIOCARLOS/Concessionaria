974fc839aee3abc4b41641c9256b44fb356920d3
SAMPAIOCARLOS <sampaiocarlos957@gmail.com>
2023-11-21 14:14:03 -0300

    new arquivos

--- script.js
+++ script.js
@@ -1,69 +1,85 @@
 const mybutton = document.getElementById('mybutton')
-
 let contador = 0
 
-mybutton.addEventListener('click', () => {
+mybutton.addEventListener('click', ()=> {
     contador ++
     if(contador > 1) {
-        alert(`Não há mais carros disponíveis.`)
         return
     }
 
-    fetch('car.json')
-    .then((response) => {
-        response.json()
-        .then((data) => {
-            const myul = document.getElementById('myul')
-            const mysection = document.getElementById('mysection')
-            const VisualizarModelo = document.querySelector('#VisualizarModelo')
-            const VisualizarAno = document.querySelector('#VisualizarAno')
-            const Volta = document.querySelector('#Volta')
+    fetch('car.json').then((response)=> {
+        return response.json()
+    }).then((data)=> {
+        const myul = document.getElementById('myul')
 
-            data.map((item) => {
-                const newli = document.createElement('li')
-                newli.innerHTML = item.marca
-
-                mysection.style.display = 'block'
-
-                VisualizarModelo.addEventListener('click', () => {
-                    newli.innerHTML = item.modelo
-                    newli.appendChild(newRemoveButton)
-                    
-                })
-
-                VisualizarAno.addEventListener('click', () => {
-                    newli.innerHTML = item.ano
-                    newli.appendChild(newRemoveButton)
-                })
-
-                Volta.addEventListener('click', () => {
-                    newli.innerHTML = item.marca
-                    newli.appendChild(newRemoveButton)
-                })
-
-                const newRemoveButton = document.createElement('button')
-                newRemoveButton.setAttribute('id', 'removeButton')
-                newRemoveButton.innerHTML = '<ion-icon name="power-outline"></ion-icon>'
-
-                newRemoveButton.addEventListener('click', () => {
-                    let confirmacao = confirm(`Deseja realmente remover esse veiculo?`)
-
-                    if(confirmacao) {
-                        myul.removeChild(newli)
-                    }
-                    
-                })
+        for(let i = 0; i < data.length; i++) {
+            const item = data[i]
 
 
-                newli.appendChild(newRemoveButton)
-                myul.appendChild(newli)
+            const newli = document.createElement('li')
+            newli.setAttribute('id', 'myli')
 
-     
-                
+            myul.appendChild(newli)
+
+            const containertext = document.createElement('div')
+            containertext.setAttribute('id', 'containertext')
+
+            newli.appendChild(containertext)
+
+            const title = document.createElement('div')
+            title.setAttribute('id', 'title')
+
+            containertext.appendChild(title)
+
+            const textMarca = document.createElement('p')
+            textMarca.setAttribute('id', 'textMarca')
+            textMarca.innerHTML = item.marca
+
+            title.appendChild(textMarca)
+
+            const buttonExibir = document.createElement('button')
+            buttonExibir.setAttribute('id', 'buttonExibir')
+            buttonExibir.innerHTML = '<ion-icon name="chevron-forward-outline"></ion-icon>'
+
+            buttonExibir.addEventListener('click', ()=> {
+                buttonExibir.classList.toggle('setabaixo');
+                buttonExibir.style.transition = '0.3s'
+                containerNames.classList.toggle('setabaixo')
+                containerNames.style.transition = '0.3s'
             })
-        
-        })
-    })
 
-   
+            title.appendChild(buttonExibir)
+
+            const containerNames = document.createElement('div')
+            containerNames.setAttribute('id', 'containerNames')
+
+            containertext.appendChild(containerNames)
+
+            const textModelo = document.createElement('p')
+            textModelo.setAttribute('id', 'textModelo')
+            textModelo.innerHTML = '- ' + item.modelo
+
+            const textAno = document.createElement('p')
+            textAno.setAttribute('id', 'textAno')
+            textAno.innerHTML = '- ' + item.ano
+
+            containerNames.appendChild(textModelo)
+            containerNames.appendChild(textAno)
+
+            const removeButton = document.createElement('button')
+            removeButton.setAttribute('id', 'removeButton')
+            removeButton.innerHTML = '<ion-icon name="power-outline"></ion-icon>'
+
+            removeButton.addEventListener('click', ()=> {
+                let confirmar = confirm(`Deseja remover esse veículo`)
+                if(confirmar) {
+                    myul.removeChild(newli)
+                }
+            })
+
+            newli.appendChild(removeButton)
+
+
+        }
+    })
 })
